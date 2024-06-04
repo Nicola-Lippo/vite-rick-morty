1fe9c5858be4558d874f27d298c14780090d82cc
Nicola-Lippo <nicolalippo8991@gmail.com>
2024-06-04 17:39:41 +0200

    [UPD] struttura base ricerca

--- src/components/HeaderSearch.vue
+++ src/components/HeaderSearch.vue
@@ -6,7 +6,16 @@ export default {
 
 <template>
     <div class="contenuto">
-        <div>sarò il componente di ricerca</div>
+        <div class="d-flex">
+            <input type="text" placeholder="cerca un personaggio">
+            <select>
+                <option value="1">alive</option>
+                <option value="2">dead</option>
+                <option value="3">unknown</option>
+            </select>
+            <button class="bottone cerca">Cerca</button>
+            <button class="bottone reset">Reset</button>
+        </div>
     </div>
 </template>
 
